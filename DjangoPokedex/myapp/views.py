0986f1a9f16bfecb8625e41d77ce4779d163e3a1
JulietteRaynaud <raynaud.juliettepro@gmail.com>
2021-11-25 08:56:26 +0100

    shiny qui marche
    shiny marche joli

--- DjangoPokedex/myapp/views.py
+++ DjangoPokedex/myapp/views.py
@@ -89,6 +89,7 @@ def init_pokemon(num_pok):
     typeFr = ''
     taille = ''
     nameFr = ''
+    shiny = ''
     if isinstance(num_pok, int):
         if 1 <= num_pok <= 898 or 10001 <= num_pok <= 10220:
             r = re.get(url + str(num_pok))
@@ -99,6 +100,7 @@ def init_pokemon(num_pok):
             image = response['sprites']['other']['official-artwork']['front_default']
             type = response['types']
             taille = int(response['height']) / 10
+            shiny = response['sprites']['front_shiny']
 
             # Traduction avec l'api des types
             typeFr = []
@@ -135,7 +137,7 @@ def init_pokemon(num_pok):
         nameFr = 'ඞ'
 
     tab_num_pok = {'nameEn': nameEn, 'img': img, 'poids': poids, 'image': image, 'type': typeFr, 'taille': taille,
-                   'nameFr': nameFr}
+                   'nameFr': nameFr, 'shiny': shiny}
     return tab_num_pok
 
 
@@ -235,7 +237,8 @@ def index(request):
                'image': tab_pok['image'], 'poids': tab_pok['poids'], 'num_pok': num_pok, 'num_pok0': num_pok0,
                'num_pok1': num_pok1,
                'type': tab_pok["type"],
-               'taille': tab_pok['taille'], 'all_pokemon': all_pokemon}
+               'taille': tab_pok['taille'], 'all_pokemon': all_pokemon,
+               'shiny': tab_pok['shiny']}
 
     return render(request, 'myapp/index.html', context)
 
